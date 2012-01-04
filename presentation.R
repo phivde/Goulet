65671e8de9f7278670840ef6048b0f0d64c45385
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2012-01-04 07:03:08 +0000

    Révision du chapitre 1 essentiellement terminée. Ne reste qu'à revoir les exercices.

--- presentation.R
+++ presentation.R
@@ -1,67 +1,67 @@
-## Emacs: -*- coding: latin-1; fill-column: 62; comment-column: 27; -*-
+## Emacs: -*- coding: utf-8; fill-column: 62; comment-column: 27; -*-
 
-### Générer deux vecteurs de nombres pseudo-aléatoires issus
-### d'une loi normale centrée réduite.
+### GÃ©nÃ©rer deux vecteurs de nombres pseudo-alÃ©atoires issus
+### d'une loi normale centrÃ©e rÃ©duite.
 x <- rnorm(50)
 y <- rnorm(x)
 
 ### Graphique des couples (x, y).
 plot(x, y)
 
-### Graphique d'une approximation de la densité du vecteur x.
+### Graphique d'une approximation de la densitÃ© du vecteur x.
 plot(density(x))
 
-### Générer la suite 1, 2, ..., 10.
+### GÃ©nÃ©rer la suite 1, 2, ..., 10.
 1:10
 
-### La fonction 'seq' sert à générer des suites plus générales.
+### La fonction 'seq' sert Ã  gÃ©nÃ©rer des suites plus gÃ©nÃ©rales.
 seq(from = -5, to = 10, by = 3)
 seq(from = -5, length = 10)
 
-### La fonction 'rep' sert à répéter des valeurs.
-rep(1, 5)        # répéter 1 cinq fois
-rep(1:5, 5)      # répéter le vecteur 1,...,5 cinq fois
-rep(1:5, each = 5) # répéter chaque élément du vecteur cinq fois
+### La fonction 'rep' sert Ã  rÃ©pÃ©ter des valeurs.
+rep(1, 5)        # rÃ©pÃ©ter 1 cinq fois
+rep(1:5, 5)      # rÃ©pÃ©ter le vecteur 1,...,5 cinq fois
+rep(1:5, each = 5) # rÃ©pÃ©ter chaque Ã©lÃ©ment du vecteur cinq fois
 
-### Arithmétique vectorielle.
+### ArithmÃ©tique vectorielle.
 v <- 1:12        # initialisation d'un vecteur
-v + 2            # additionner 2 à chaque élément de v
-v * -12:-1       # produit élément par élément
-v + 1:3          # le vecteur le plus court est recyclé
+v + 2            # additionner 2 Ã  chaque Ã©lÃ©ment de v
+v * -12:-1       # produit Ã©lÃ©ment par Ã©lÃ©ment
+v + 1:3          # le vecteur le plus court est recyclÃ©
 
 ### Vecteur de nombres uniformes sur l'intervalle [1, 10].
 v <- runif(12, min = 1, max = 10)
 v
 
-### Pour afficher le résultat d'une affectation, placer la
-### commande entre parenthèses.
+### Pour afficher le rÃ©sultat d'une affectation, placer la
+### commande entre parenthÃ¨ses.
 ( v <- runif(12, min = 1, max = 10) )
 
-### Arrondi des valeurs de v à l'entier près.
+### Arrondi des valeurs de v Ã  l'entier prÃ¨s.
 ( v <- round(v) )
 
-### Créer une matrice 3 x 4 à partir des valeurs de
+### CrÃ©er une matrice 3 x 4 Ã  partir des valeurs de
 ### v. Remarquer que la matrice est remplie par colonne.
 ( m <- matrix(v, nrow = 3, ncol = 4) )
 
-### Les opérateurs arithmétiques de base s'appliquent aux
+### Les opÃ©rateurs arithmÃ©tiques de base s'appliquent aux
 ### matrices comme aux vecteurs.
 m + 2
 m * 3
 m ^ 2
 
-### Éliminer la quatrième colonne afin d'obtenir une matrice
-### carrée.
+### Ã‰liminer la quatriÃ¨me colonne afin d'obtenir une matrice
+### carrÃ©e.
 ( m <- m[,-4] )
 
-### Transposée et inverse de la matrice m.
+### TransposÃ©e et inverse de la matrice m.
 t(m)
 solve(m)
 
 ### Produit matriciel.
-m %*% m               # produit de m avec elle-même
+m %*% m               # produit de m avec elle-mÃªme
 m %*% solve(m)        # produit de m avec son inverse
-round(m %*% solve(m)) # l'arrondi donne la matrice identité
+round(m %*% solve(m)) # l'arrondi donne la matrice identitÃ©
 
 ### Consulter la rubrique d'aide de la fonction 'solve'.
 ?solve
