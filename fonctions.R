612c5478265b475f503fc1786b16c3af82e2e4a6
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2012-01-09 03:25:42 +0000

    Chapitre 5 entièrement révisé.

--- fonctions.R
+++ fonctions.R
@@ -1,11 +1,13 @@
-## Emacs: -*- coding: latin-1; fill-column: 62; comment-column: 27; -*-
+## Emacs: -*- coding: utf-8; fill-column: 62; comment-column: 27; -*-
 
-### Premier exemple de fonction: la mise en oeuvre de
-### l'algorithme du point fixe pour trouver le taux d'intérêt
-### tel que a_angle{n} = k pour 'n' et 'k' donnés. Cette mise
-### en oeuvre est peu générale puisqu'il faudrait modifier la
-### fonction chaque fois que l'on change la fonction f(x)
-### dont on cherche le point fixe.
+### POINT FIXE
+
+## Comme premier exemple de fonction, on rÃ©alise une mise en
+## oeuvre de l'algorithme du point fixe pour trouver le taux
+## d'intÃ©rÃªt tel que a_angle{n} = k pour 'n' et 'k' donnÃ©s.
+## Cette mise en oeuvre est peu gÃ©nÃ©rale puisqu'il faudrait
+## modifier la fonction chaque fois que l'on change la
+## fonction f(x) dont on cherche le point fixe.
 fp1 <- function(k, n, start = 0.05, TOL = 1E-10)
 {
     i <- start
@@ -19,19 +21,21 @@ fp1 <- function(k, n, start = 0.05, TOL = 1E-10)
     i
 }
 
-fp1(7.2, 10)               # valeur de départ par défaut
-fp1(7.2, 10, 0.06)         # valeur de départ spécifiée
-i                          # les variables n'existent pas
-start                      # dans l'espace de travail
+fp1(7.2, 10)               # valeur de dÃ©part par dÃ©faut
+fp1(7.2, 10, 0.06)         # valeur de dÃ©part spÃ©cifiÃ©e
+i                          # les variables n'existent pas...
+start                      # ... dans l'espace de travail
 
-### Second exemple: généralisation de la fonction 'fp1' où la
-### fonction f(x) dont on cherche le point fixe (c'est-à-dire
-### la valeur de 'x' tel que f(x) = x) est passée en
-### argument. On peut faire ça? Bien sûr, puisqu'une fonction
-### est un objet comme un autre en S. On ajoute également à
-### la fonction un argument 'echo' qui, lorsque TRUE, fera
-### en sorte d'afficher à l'écran les valeurs successives
-### de 'x'.
+## GÃ©nÃ©ralisation de la fonction 'fp1': la fonction f(x) dont
+## on cherche le point fixe (c'est-Ã -dire la valeur de 'x'
+## tel que f(x) = x) est passÃ©e en argument. On peut faire
+## Ã§a? Bien sÃ»r, puisqu'une fonction est un objet comme un
+## autre en R. On ajoute Ã©galement Ã  la fonction un argument
+## 'echo' qui, lorsque TRUE, fera en sorte d'afficher Ã 
+## l'Ã©cran les valeurs successives de 'x'.
+##
+## Ci-dessous, il est implicite que le premier argument, FUN,
+## est une fonction.
 fp2 <- function(FUN, start, echo = FALSE, TOL = 1E-10)
 {
     x <- start
@@ -39,10 +43,10 @@ fp2 <- function(FUN, start, echo = FALSE, TOL = 1E-10)
     {
         xt <- x
 
-        if (echo)  # inutile de faire 'if (echo == TRUE)'
+        if (echo)       # inutile de faire 'if (echo == TRUE)'
             print(xt)
 
-        x <- FUN(xt)
+        x <- FUN(xt)    # appel de la fonction
 
         if (abs(x - xt)/xt < TOL)
             break
@@ -50,22 +54,26 @@ fp2 <- function(FUN, start, echo = FALSE, TOL = 1E-10)
     x
 }
 
-f <- function(i) (1 - (1+i)^(-10))/7.2 # définition de f(x)
+f <- function(i) (1 - (1+i)^(-10))/7.2 # dÃ©finition de f(x)
 fp2(f, 0.05)               # solution
-fp2(f, 0.05, echo = TRUE)  # avec résultats intermédiaires
-fp2(function(x) 3^(-x), start = 0.5) # avec une fonction anonyme
+fp2(f, 0.05, echo = TRUE)  # avec rÃ©sultats intermÃ©diaires
+fp2(function(x) 3^(-x), start = 0.5) # avec fonction anonyme
 
-### Troisième exemple: amélioration mineure à la fonction
-### 'fp2'. Puisque la valeur de 'echo' ne change pas pendant
-### l'exécution de la fonction, on peut éviter de refaire le
-### test à chaque itération de la boucle. Une solution
-### élégante consiste à utiliser un outil avancé du langage S:
-### les expressions. On se contentera d'une illustration ici,
-### sans entrer dans les détails.
+## AmÃ©lioration mineure Ã  la fonction 'fp2': puisque la
+## valeur de 'echo' ne change pas pendant l'exÃ©cution de la
+## fonction, on peut Ã©viter de refaire le test Ã  chaque
+## itÃ©ration de la boucle. Une solution Ã©lÃ©gante consiste Ã 
+## utiliser un outil avancÃ© du langage R: les expressions.
+##
+## L'objet crÃ©Ã© par la fonction 'expression' est une
+## expression non encore Ã©valuÃ©e (comme si on n'avait pas
+## appuyÃ© sur EntrÃ©e Ã  la fin de la ligne). On peut ensuite
+## Ã©valuer l'expression (appuyer sur EntrÃ©e) avec 'exec'.
 fp3 <- function(FUN, start, echo = FALSE, TOL = 1E-10)
 {
     x <- start
 
+    ## Choisir l'expression Ã  exÃ©cuter plus loin
     if (echo)
         expr <- expression(print(xt <- x))
     else
@@ -73,9 +81,9 @@ fp3 <- function(FUN, start, echo = FALSE, TOL = 1E-10)
 
     repeat
     {
-        eval(expr)
+        eval(expr)         # Ã©valuer l'expression
 
-        x <- FUN(xt)
+        x <- FUN(xt)       # appel de la fonction
 
         if (abs(x - xt)/xt < TOL)
             break
@@ -83,21 +91,18 @@ fp3 <- function(FUN, start, echo = FALSE, TOL = 1E-10)
     x
 }
 
-fp3(f, 0.05, echo = TRUE)    # avec résultats intermédiaires
+fp3(f, 0.05, echo = TRUE)  # avec rÃ©sultats intermÃ©diaires
 fp3(function(x) 3^(-x), start = 0.5) # avec une fonction anonyme
 
-### La suite de Fibonacci (et son lien avec le nombre d'or) a
-### été remise au goût du jour par le bestseller «Code Da
-### Vinci». Les valeurs de la suite de Fibonacci sont données
-### par la fonction suivante:
-###
-###      f(0) = 0
-###      f(1) = 1
-###      f(n) = f(n - 1) + f(n - 2), n >= 2.
-###
-### Voici deux exemples de fonctions calculant la suite de
-### Fibonacci. La première calcule les 'n' premières valeurs
-### de la série.
+### SUITE DE FIBONACCI
+
+## On a prÃ©sentÃ© au chapitre 4 deux maniÃ¨res diffÃ©rentes de
+## pour calculer les 'n' premiÃ¨res valeurs de la suite de
+## Fibonacci. On crÃ©e d'abord des fonctions Ã  partir de ce
+## code. Avantage d'avoir des fonctions: elles sont valides
+## pour tout 'n' > 2.
+##
+## D'abord la version inefficace.
 fib1 <- function(n)
 {
     res <- c(0, 1)
@@ -108,43 +113,33 @@ fib1 <- function(n)
 fib1(10)
 fib1(20)
 
-### La fonction 'fib1' a un gros défaut: la taille de l'objet
-### 'res' est constamment augmentée pour stocker une nouvelle
-### valeur de la série. Cela coûte très cher en S et doit
-### absolument être évité lorsque c'est possible (et ce l'est
-### la plupart du temps). Quand on sait quelle sera la
-### longueur d'un objet (comme c'est le cas ici), il vaut
-### mieux créer un contenant vide de la bonne longueur et le
-### remplir par la suite.
+## Puis la version supposÃ©ement plus efficace.
 fib2 <- function(n)
 {
-    res <- numeric(n)      # contenant créé
-    res[2] <- 1            # res[1] vaut déjà 0
+    res <- numeric(n)      # contenant crÃ©Ã©
+    res[2] <- 1            # res[1] vaut dÃ©jÃ  0
     for (i in 3:n)
         res[i] <- res[i - 1] + res[i - 2]
     res
 }
-fib2(10)
+fib2(5)
 fib2(20)
 
-### A-t-on vraiment gagné quelque chose? Comparons le temps
-### requis pour générer une longue suite de Fibonacci avec les
-### deux fonctions. (En fait, le gain est beaucoup plus
-### important avec R qu'avec S-Plus.)
-sys.time(fib1(10000))      # S-Plus seulement
-sys.time(fib2(10000))      # S-Plus seulement
-system.time(fib1(10000))   # R seulement
-system.time(fib2(10000))   # R seulement
+## A-t-on vraiment gagnÃ© en efficacitÃ©? Comparons le temps
+## requis pour gÃ©nÃ©rer une longue suite de Fibonacci avec les
+## deux fonctions.
+system.time(fib1(10000))   # version inefficace
+system.time(fib2(10000))   # version efficace
 
-### Second exemple basé sur la suite de Fibonacci: une
-### fonction pour calculer non pas les 'n' premières valeurs
-### de la suite, mais uniquement la 'n'ième valeur.
-###
-### Mais il y a un mais: la fonction 'fib3' est truffée
-### d'erreurs (de syntaxe, d'algorithmique, de conception). À
-### vous de trouver les bogues. (Afin de préserver cet
-### exemple, copier le code erroné plus bas ou dans un autre
-### fichier avant d'y faire les corrections.)
+## Variation sur un mÃªme thÃ¨me: une fonction pour calculer non
+## pas les 'n' premiÃ¨res valeurs de la suite de Fibonacci,
+## mais uniquement la 'n'iÃ¨me valeur.
+##
+## Mais il y a un mais: la fonction 'fib3' est truffÃ©e
+## d'erreurs (de syntaxe, d'algorithmique, de conception). Ã€
+## vous de trouver les bogues. (Afin de prÃ©server cet
+## exemple, copier le code erronÃ© plus bas ou dans un autre
+## fichier avant d'y faire les corrections.)
 fib3 <- function(nb)
 {
     x <- 0
