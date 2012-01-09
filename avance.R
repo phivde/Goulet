118d52f109422ecddb2409d928db03ff3f66b335
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2012-01-09 06:25:33 +0000

    Chapitre 1-6 entièrement révisés.

--- avance.R
+++ avance.R
@@ -1,21 +1,21 @@
-## Emacs: -*- coding: latin-1; fill-column: 62; comment-column: 27; -*-
+## Emacs: -*- coding: utf-8; fill-column: 62; comment-column: 27; -*-
 
 ###
 ### FONCTION 'apply'
 ###
 
-## Création d'une matrice et d'un tableau à trois dimensions
+## CrÃ©ation d'une matrice et d'un tableau Ã  trois dimensions
 ## pour les exemples.
 m <- matrix(sample(1:100, 20), nrow = 4, ncol = 5)
 a <- array(sample(1:100, 60), dim = 3:5)
 
 ## Les fonctions 'rowSums', 'colSums', 'rowMeans' et
 ## 'colMeans' sont des raccourcis pour des utilisations
-## fréquentes de 'apply'.
-rowSums(m)
-apply(m, 1, sum)
-colMeans(m)
-apply(m, 2, mean)
+## frÃ©quentes de 'apply'.
+rowSums(m)                 # somme par ligne
+apply(m, 1, sum)           # idem, mais moins lisible
+colMeans(m)                # somme par colonne
+apply(m, 2, mean)          # idem, mais moins lisible
 
 ## Puisqu'il n'existe pas de fonctions comme 'rowMax' ou
 ## 'colProds', il faut utiliser 'apply'.
@@ -23,152 +23,300 @@ apply(m, 1, max)           # maximum par ligne
 apply(m, 2, prod)          # produit par colonne
 
 ## L'argument '...' de 'apply' permet de passer des arguments
-## à la fonction FUN.
-m[sample(1:20, 5)] <- NA   # ajout de données manquantes
+## Ã  la fonction FUN.
+m[sample(1:20, 5)] <- NA   # ajout de donnÃ©es manquantes
 apply(m, 1, var, na.rm = TRUE) # variance par ligne sans NA
 
-## Lorsque 'apply' est utilisée sur un tableau, son résultat
-## est de dimensions dim(X)[MARGIN].
-apply(a, c(2, 3), sum)     # le résultat est une matrice
-apply(a, 1, prod)          # le résultat est un vecteur
+## Lorsque 'apply' est utilisÃ©e sur un tableau, son rÃ©sultat
+## est de dimensions dim(X)[MARGIN], d'oÃ¹ le truc
+## mnÃ©motechnique donnÃ© dans le texte du chapitre.
+apply(a, c(2, 3), sum)     # le rÃ©sultat est une matrice
+apply(a, 1, prod)          # le rÃ©sultat est un vecteur
+
+## L'utilisation de 'apply' avec les tableaux peut rapidement
+## devenir confondante si l'on ne visualise pas les calculs
+## qui sont rÃ©alisÃ©s. On reprend ici les exemples du chapitre
+## en montrant comment l'on calculerait le premier Ã©lÃ©ment de
+## chaque utilisation de 'apply'. Au besoin, retourner Ã 
+## l'indiÃ§age des tableaux au chapitre 2.
+(x <- array(sample(1:10, 80, rep = TRUE), c(3, 3, 4)))
+apply(x, 3, det)      # dÃ©terminants des quatre matrices 3 x 3
+det(x[, , 1])         # Ã©quivalent pour le premier dÃ©terminant
+
+apply(x, 1, sum)      # sommes des trois tranches horizontales
+sum(x[1, , ])         # Ã©quivalent pour la premiÃ¨re somme
+
+apply(x, c(1, 2), sum) # sommes des neuf carottes horizontales
+sum(x[1, 1, ])         # Ã©quivalent pour la premiÃ¨re somme
+
+apply(x, c(1, 3), sum) # sommes des 12 carottes transversales
+sum(x[1, , 1])         # Ã©quivalent pour la premiÃ¨re somme
+
+apply(x, c(2, 3), sum) # sommes des 12 carottes verticales
+sum(x[, 1, 1])         # Ã©quivalent pour la premiÃ¨re somme
 
 ###
 ### FONCTIONS 'lapply' ET 'sapply'
 ###
 
-## La fonction 'lapply' applique une fonction à tous les
-## éléments d'une liste et retourne une liste, peu importe les
-## dimensions des résultats. La fonction 'sapply' retourne un
+## La fonction 'lapply' applique une fonction Ã  tous les
+## Ã©lÃ©ments d'une liste et retourne une liste, peu importe les
+## dimensions des rÃ©sultats. La fonction 'sapply' retourne un
 ## vecteur ou une matrice, si possible.
 ##
-## Somme «interne» des éléments d'une liste.
-( liste <- list(1:10,
-                c(-2, 5, 6),
-                matrix(3, 4, 5)) )
-sum(liste)                 # erreur
-lapply(liste, sum)         # sommes internes (liste)
-sapply(liste, sum)         # sommes internes (vecteur)
+## Somme Â«interneÂ» des Ã©lÃ©ments d'une liste.
+(x <- list(1:10, c(-2, 5, 6), matrix(3, 4, 5)))
+sum(x)                     # erreur
+lapply(x, sum)             # sommes internes (liste)
+sapply(x, sum)             # sommes internes (vecteur)
 
-## Création de la suite 1, 1, 2, 1, 2, 3, 1, 2, 3, 4, ..., 1,
+## CrÃ©ation de la suite 1, 1, 2, 1, 2, 3, 1, 2, 3, 4, ..., 1,
 ## 2, ..., 9, 10.
-lapply(1:10, seq)          # le résultat est une liste
-unlist(lapply(1:10, seq))  # le résultat est un vecteur
+lapply(1:10, seq)          # le rÃ©sultat est une liste
+unlist(lapply(1:10, seq))  # le rÃ©sultat est un vecteur
 
-## Soit une fonction calculant la moyenne pondérée d'un
+## Soit une fonction calculant la moyenne pondÃ©rÃ©e d'un
 ## vecteur. Cette fonction prend en argument une liste de deux
-## éléments: 'donnees' et 'poids'.
+## Ã©lÃ©ments: 'donnees' et 'poids'.
 fun <- function(liste)
     sum(liste$donnees * liste$poids)/sum(liste$poids)
 
-## On peut maintenant calculer la moyenne pondérée de
-## plusieurs ensembles de données réunis dans une liste
-## itérée.
-( a <- list(list(donnees = 1:7,
-                 poids = (5:11)/56),
-            list(donnees = sample(1:100, 12),
-                 poids = 1:12),
-            list(donnees = c(1, 4, 0, 2, 2),
-                 poids = c(12, 3, 17, 6, 2))) )
-sapply(a, fun)
+## On peut maintenant calculer la moyenne pondÃ©rÃ©e de
+## plusieurs ensembles de donnÃ©es rÃ©unis dans une liste
+## itÃ©rÃ©e.
+(x <- list(list(donnees = 1:7,
+                poids = (5:11)/56),
+           list(donnees = sample(1:100, 12),
+                poids = 1:12),
+           list(donnees = c(1, 4, 0, 2, 2),
+                poids = c(12, 3, 17, 6, 2))))
+sapply(x, fun)             # aucune boucle explicite!
 
 ###
 ### FONCTION 'mapply'
 ###
 
-## Création de quatre échantillons aléatoires de taille 12.
-a <- lapply(rep(12, 4), sample, x = 1:100)
+## CrÃ©ation de quatre Ã©chantillons alÃ©atoires de taille 12.
+x <- lapply(rep(12, 4), sample, x = 1:100)
 
-## Moyennes tronquées à 0, 10, 20 et 30%, respectivement, de
-## ces quatre échantillons aléatoires.
-mapply(mean, a, 0:3/10)
+## Moyennes tronquÃ©es Ã  0, 10, 20 et 30%, respectivement, de
+## ces quatre Ã©chantillons alÃ©atoires.
+mapply(mean, x, 0:3/10)
 
 ###
 ### FONCTION 'replicate'
 ###
 
-## La fonction 'replicate' va répéter un certain nombre de
+## La fonction 'replicate' va rÃ©pÃ©ter un certain nombre de
 ## fois une expression quelconque. Le principal avantage de
-## 'replicate' sur 'sapply' est qu'on n'a pas à se soucier des
-## arguments à passer à une fonction.
+## 'replicate' sur 'sapply' est qu'on n'a pas Ã  se soucier des
+## arguments Ã  passer Ã  une fonction.
 ##
-## Par exemple, on veut simuler dix échantillons aléatoires
-## indépendants de longueur 12. On peut utiliser 'sapply',
-## mais la syntaxe n'est ni élégante, ni facile à lire
-## (l'argument 'i' ne sert à rien).
+## Par exemple, on veut simuler dix Ã©chantillons alÃ©atoires
+## indÃ©pendants de longueur 12. On peut utiliser 'sapply',
+## mais la syntaxe n'est ni Ã©lÃ©gante, ni facile Ã  lire
+## (l'argument 'i' ne sert Ã  rien).
 sapply(rep(1, 10), function(i) sample(1:100, 12))
 
 ## En utilisant 'replicate', on sait tout de suite de quoi il
-## s'agit. À noter que les échantillons se trouvent dans les
-## colonnes de la matrice résultante.
+## s'agit. Ã€ noter que les Ã©chantillons se trouvent dans les
+## colonnes de la matrice rÃ©sultante.
 replicate(10, sample(1:100, 12))
 
-## Vérification que la moyenne arithmétique (bar{X}) est un
+## VÃ©rification que la moyenne arithmÃ©tique (bar{X}) est un
 ## estimateur sans biais de la moyenne de la loi normale. On
-## doit calculer la moyenne de plusieurs échantillons
-## aléatoires, puis la moyenne de toutes ces moyennes.
+## doit calculer la moyenne de plusieurs Ã©chantillons
+## alÃ©atoires, puis la moyenne de toutes ces moyennes.
 ##
-## On définit d'abord une fonction pour faire une simulation.
+## On dÃ©finit d'abord une fonction pour faire une simulation.
+## Remarquer que dans la fonction ci-dessous, 'mean' est tour
+## Ã  tour le nom d'un argument (qui pourrait aussi bien Ãªtre
+## Â«totoÂ») et la fonction pour calculer une moyenne.
 fun <- function(n, mean, sd)
     mean(rnorm(n, mean = mean, sd = sd))
 
 ## Avec 'replicate', on fait un grand nombre de simulations.
-res <- replicate(10000, fun(100, 0, 1)) # 10000 simulations
-hist(res)                  # distribution de bar{X}
-mean(res)                  # moyenne de bar{X}
+x <- replicate(10000, fun(100, 0, 1)) # 10000 simulations
+hist(x)                    # distribution de bar{X}
+mean(x)                    # moyenne de bar{X}
 
 ###
-### CLASSES ET FONCTIONS GÉNÉRIQUES
+### CLASSES ET FONCTIONS GÃ‰NÃ‰RIQUES
 ###
 
-## Afin d'illustrer l'utilisation des classes et des fonctions
-## génériques, nous allons créer une classe 'toto' et une
-## méthode de la fonction générique 'print' pour cette classe.
+## Pour illustrer les classes et fonctions gÃ©nÃ©riques, on
+## reprend la fonction de point fixe 'fp3' des exemples du
+## chapitre 5 en y faisant deux modifications:
 ##
-## Si la fonction 'print' est appelée avec un objet de mode
-## 'numeric' et de classe 'toto', c'est le résultat de la
-## fonction 'diag' qui est retourné. (Ne pas chercher un sens
-## caché à tout ça, il n'y en a pas.)
+##   1. ajout d'un compteur pour le nombre d'itÃ©rations;
+##   2. la fonction retourne une liste de classe 'fp'
+##      contenant diverses informations relatives Ã  la
+##      procÃ©dure de point fixe.
 ##
-## Définition de la nouvelle méthode.
-print.toto <- function(x)
+## Ainsi, la fonction 'fp4' retourne un objet qui peut ensuite
+## Ãªtre manipulÃ© par des mÃ©thodes de fonctions gÃ©nÃ©riques.
+## C'est l'approche de programmation objet favorisÃ©e dans le
+## langage R.
+fp4 <- function(FUN, start, echo = FALSE, TOL = 1E-10)
 {
-    if (mode(x) == "numeric")
-    {
-        cat("\n  Resultat de 'diag':\n")
-        print(diag(x))
-    }
+    x <- start             # valeur de dÃ©part
+    i <- 0                 # compteur des itÃ©rations
+
+    if (echo)
+        expr <- expression(print(xt <- x))
     else
-        print.default(x)
+        expr <- expression(xt <- x)
+
+    repeat
+    {
+        eval(expr)
+
+        x <- FUN(xt)       # nouvelle valeur
+        i <- i + 1         # incrÃ©menter le compteur
+
+        if (abs(x - xt)/xt < TOL)
+            break
+    }
+
+    structure(list(fixed.point = x, # point fixe
+                   nb.iter = i,     # nombre d'itÃ©rations
+                   fun = FUN,       # fonction f(x)
+                   x0 = start,      # valeur de dÃ©part
+                   TOL = TOL),      # prÃ©cision relative
+              class = "fp")
 }
 
-## Vérification que la méthode est disponible.
-methods(print)
+## On crÃ©e maintenant des mÃ©thodes pour la classe 'fp' pour
+## les fonctions gÃ©nÃ©riques les plus courantes, soit 'print',
+## 'summary' et 'plot'.
+##
+## La mÃ©thode de 'print' sera utilisÃ©e pour afficher seulement
+## la valeur du point fixe. C'est en quelque sorte
+## l'utilisation la plus simple de la fonction 'fp4'.
+##
+## La mÃ©thode de 'summary' fournira un peu plus d'informations
+## sur la procÃ©dure de point fixe.
+##
+## Enfin, la mÃ©thode de 'plot' fera un graphique de la
+## fonction f(x) et son intersection avec la droite y = x.
+print.fp <- function(x)
+    print(x$fixed.point)
 
-## Essai de la nouvelle méthode sur un scalaire.
-x <- 4
-class(x)                   # classe par défaut
-x                          # méthode par défaut
-class(x) <- "toto"         # objet de classe 'toto'
-x                          # méthode pour cette classe
+summary.fp <- function(x)
+{
+    if (class(x) != "fp")
+        stop("object is not of class 'fp'")
+    cat("Function:\n ")
+    print(x$fun)
+    cat("\n")
+    cat("Fixed point:\n ", x$fixed.point, fill = TRUE)
+    cat("\n")
+    cat("Number of iterations:\n ", x$nb.iter, fill = TRUE)
+    cat("\n")
+    cat("Precision:\n ", x$TOL, fill = TRUE)
+}
 
-## Essai de la nouvelle méthode sur un vecteur.
-x <- 1:5
-class(x)                   # classe par défaut
-x                          # méthode par défaut
-class(x) <- "toto"         # objet de classe 'toto'
-x                          # méthode pour cette classe
+plot.fp <- function(x, ...)
+{
+    ## Valeur du point fixe
+    fp <- x$fixed.point
 
-## Essai de la nouvelle méthode sur une matrice. Les matrices
-## ont une classe "matrix" implicite.
-x <- matrix(1:9, 3, 3)
-class(x)                   # classe implicite
-x                          # méthode par défaut
-class(x) <- "toto"         # objet de classe 'toto'
-x                          # méthode pour cette classe
+    ## Il faut dÃ©terminer un intervalle pour lequel tracer la
+    ## fonction. Celui-ci est dÃ©terminÃ© de faÃ§on arbitraire
+    ## comme un multiple de la distance entre la valeur de
+    ## dÃ©part et la valeur du point fixe.
+    r <- abs(x$x0 - fp)
 
-## La nouvelle méthode ne fait rien de spécial pour les objets
-## d'un mode autre que 'numeric'.
-x <- letters
-mode(x)
-class(x) <- "toto"
-x
+    ## Fonction Ã  tracer
+    FUN <- x$fun
+
+    ## Fonction y = x. 'FUN2' est nÃ©cessaire parce que 'curve'
+    ## n'admet pas de fonctions anonymes en argument.
+    FUN2 <- function(x) x
+
+    ## Graphique de la fonction 'FUN'
+    curve(FUN, from = fp - 3 * r, to = fp + 3 * r,
+          xlab = "x", ylab = "f(x)", lwd = 2)
+
+    ## Ajout de la droite 'FUN2' au graphique
+    curve(FUN2, add = TRUE, lwd = 1)
+
+    ## Ajout d'un point sur le point fixe
+    points(fp, FUN(fp), ...)
+}
+
+## Exemples d'utilisation
+x <- fp4(function(x) 3^(-x), start = 0.5)
+x                          # affichage de 'print.fp'
+summary(x)                 # plus d'information
+plot(x)                    # graphique de base
+plot(x, pch = 21,          # graphique plus Ã©laborÃ©...
+     bg = "orange",        # ... consulter la rubrique
+     cex = 2, lwd = 2)     # ... d'aide de 'par'
+
+###
+### OPÃ‰RATEURS EN TANT QUE FONCTIONS
+###
+
+## Les opÃ©rateurs reprÃ©sentÃ©s par des caractÃ¨res spÃ©ciaux sont
+## des fonctions comme les autres. On peut donc les appeler
+## comme toute autre fonction. (En fait, l'interprÃ¨te R fait
+## cette traduction Ã  l'interne.)
+x <- sample(1:100, 12)     # un vecteur
+x + 2                      # appel usuel
+"+"(x, 2)                  # Ã©quivalent
+x[c(3, 5)]                 # extraction usuelle
+"["(x, c(3, 5))            # Ã©quivalent
+x[1] <- 0; x               # assignation usuelle
+"[<-"(x, 2, 0)             # Ã©quivalent (Ã  x[2] <- 0)
+
+## D'une part, cela explique pourquoi il faut placer les
+## opÃ©rateurs entre guillemets (" ") lorsqu'on les utilise
+## dans les fonctions comme 'outer', 'lapply', etc.
+outer(x, x, +)             # erreur de syntaxe
+outer(x, x, "+")           # correct
+
+## D'autre part, cela permet d'utiliser les opÃ©rateurs
+## d'extraction "[" et "[[" dans de telles fonctions. Par
+## exemple, voici comment extraire le deuxiÃ¨me Ã©lÃ©ment de
+## chaque Ã©lÃ©ment d'une liste.
+(x <- list(1:4, 8:2, 6:12, -2:2)) # liste quelconque
+x[[1]][2]                  # 2e Ã©lÃ©ment du 1er Ã©lÃ©ment
+x[[2]][2]                  # 2e Ã©lÃ©ment du 2e Ã©lÃ©ment
+x[[3]][2]                  # 2e Ã©lÃ©ment du 3e Ã©lÃ©ment
+x[[4]][2]                  # 2e Ã©lÃ©ment du 4e Ã©lÃ©ment
+lapply(x, "[", 2)          # mÃªme chose en une ligne
+sapply(x, "[", 2)          # rÃ©sultat sous forme de vecteur
+
+###
+### COMMENT JOUER DES TOURS AVEC R
+###
+
+## RedÃ©finir un opÃ©rateur dans l'espace de travail de
+## quelqu'un...
+"+" <- function(x, y) x * y # redÃ©finition de "+"
+5 + 2                      # ouch!
+ls()                       # traÃ®trise dÃ©voilÃ©e...
+rm("+")                    # ... puis Ã©liminÃ©e
+5 + 2                      # c'est mieux
+
+## Faire croire qu'une fonction fait autre chose que ce
+## qu'elle fait en rÃ©alitÃ©. Si l'attribut "source" d'une
+## fonction existe, c'est son contenu qui est affichÃ© lorsque
+## l'on examine une fonction.
+f <- function(x, y) x + y  # vraie fonction
+attr(f, "source") <- "function(x, y) x * y" # ce qui est affichÃ©
+f                          # une fonction pour faire le produit?
+f(2, 3)                    # non!
+str(f)                     # structure de l'objet
+attr(f, "source") <- NULL  # attribut "source" effacÃ©
+f                          # c'est mieux
+
+## RedÃ©finir la mÃ©thode de 'print' pour une classe d'objet...
+## Ici, l'affichage d'un objet de classe "lm" cause la
+## fermeture de R!
+print.lm <- function(x) q("ask")
+
+x <- rnorm(10)             # Ã©chantillon alÃ©atoire
+y <- x + 2 + rnorm(10)     # modÃ¨le de rÃ©gression linÃ©aire
+lm(y ~ x)                  # rÃ©pondre "c"!
