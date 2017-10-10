5ba343da869ac0ffd4aaf6fe40e1b506f1b89cc6
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2017-10-10 17:56:20 -0400

    exemples jusqu'à import-export de données

---
+++ donnees.R
@@ -0,0 +1,255 @@
+### Emacs: -*- coding: utf-8; fill-column: 62; comment-column: 27; -*-
+##
+## Copyright (C) 2017 Vincent Goulet
+##
+## Ce fichier fait partie du projet «Programmer avec R»
+## http://github.com/vigou3/programmer-avec-r
+##
+## Cette création est mise à disposition selon le contrat
+## Attribution-Partage dans les mêmes conditions 4.0
+## International de Creative Commons.
+## http://creativecommons.org/licenses/by-sa/4.0/
+
+###
+### MATRICE ET TABLEAU
+###
+
+## Une matrice est un vecteur avec un attribut 'dim' de
+## longueur 2 et une classe implicite "matrix". La manière
+## naturelle de créer une matrice est avec la fonction
+## 'matrix'.
+(x <- matrix(1:12, nrow = 3, ncol = 4))
+length(x)                  # longueur du vecteur sous-jacent
+attributes(x)              # objet muni d'un attribut 'dim'
+dim(x)                     # deux dimensions
+
+## Les matrices sont remplies par colonne par défaut. L'option
+## 'byrow' permet de les remplir par ligne, si nécessaire.
+matrix(1:12, nrow = 3, ncol = 4, byrow = TRUE)
+
+## Il n'est pas nécessaire de préciser les deux dimensions de
+## la matrice s'il est possible d'en déduire une à partir du
+## vecteur de données. Les expressions ci-dessous sont toutes
+## équivalentes.
+matrix(1:12, nrow = 3, ncol = 4)
+matrix(1:12, nrow = 3)
+matrix(1:12, ncol = 4)
+
+## À l'inverse, s'il n'y a pas assez de données pour remplir
+## les dimensions précisées, les données seront recyclées,
+## comme d'habitude.
+matrix(1, nrow = 3, ncol = 4)
+matrix(1:3, nrow = 3, ncol = 4)
+matrix(1:4, nrow = 3, ncol = 4, byrow = TRUE)
+
+## Dans l'indiçage des matrices et tableaux, l'indice de
+## chaque dimension obéit aux règles usuelles d'indiçage des
+## vecteurs.
+x[1, 2]                    # élément en position (1, 2)
+x[1, -2]                   # 1ère rangée sans 2e colonne
+x[c(1, 3), ]               # 1ère et 3e rangées
+x[-1, ]                    # supprimer 1ère rangée
+x[, -2]                    # supprimer 2e colonne
+x[x[, 1] > 2, ]            # lignes avec 1er élément > 2
+
+## Indicer la matrice ou le vecteur sous-jacent est
+## équivalent. Utiliser l'approche la plus simple selon le
+## contexte.
+x[1, 3]                    # l'élément en position (1, 3)...
+x[7]                       # ... est le 7e élément du vecteur
+
+## Détail additionnel sur l'indiçage des matrices et tableaux:
+## il est aussi possible de les indicer avec une matrice.
+## Chaque ligne de la matrice d'indiçage fournit alors la
+## position d'un élément à sélectionner.
+##
+## Consulter au besoin la rubrique d'aide de la fonction '['
+## (ou de 'Extract').
+x[rbind(c(1, 1), c(2, 2))] # éléments x[1, 1] et x[2, 2]
+x[cbind(1:3, 1:3)]         # éléments x[i, i] («diagonale»)
+diag(x)                    # idem et plus explicite
+
+## Quelques fonctions pour travailler avec les dimensions des
+## matrices.
+nrow(x)                    # nombre de lignes
+dim(x)[1]                  # idem
+ncol(x)                    # nombre de colonnes
+dim(x)[2]                  # idem
+
+## La fonction 'rbind' ("row bind") permet d'«empiler» des
+## matrices comptant le même nombre de colonnes.
+##
+## De manière similaire, la fonction 'cbind' ("column bind")
+## permet de concaténer des matrices comptant le même nombre de
+## lignes.
+##
+## Utilisées avec un seul argument, 'rbind' et 'cbind' créent
+## des vecteurs ligne et colonne, respectivement. Ceux-ci sont
+## rarement nécessaires.
+x <- matrix(1:12, 3, 4)    # 'x' est une matrice 3 x 4
+y <- matrix(1:8, 2, 4)     # 'y' est une matrice 2 x 4
+z <- matrix(1:6, 3, 2)     # 'z' est une matrice 3 x 2
+rbind(x, 99)               # ajout d'une ligne à 'x'
+rbind(x, y)                # fusion verticale de 'x' et 'y'
+cbind(x, 99)               # ajout d'une colonne à 'x'
+cbind(x, z)                # concaténation de 'x' et 'z'
+rbind(x, z)                # dimensions incompatibles
+cbind(x, y)                # dimensions incompatibles
+rbind(1:3)                 # vecteur ligne
+cbind(1:3)                 # vecteur colonne
+
+## Un tableau (array) est un vecteur avec plus de deux
+## dimensions. Pour le reste, la manipulation des tableaux
+## est en tous points identique à celle des matrices. Ne pas
+## oublier: les tableaux sont remplis de la première dimension
+## à la dernière!
+(x <- array(1:60, 3:5))    # tableau 3 x 4 x 5
+length(x)                  # longueur du vecteur sous-jacent
+dim(x)                     # trois dimensions
+x[1, 3, 2]                 # l'élément en position (1, 3, 2)...
+x[19]                      # ... est le 19e élément du vecteur
+
+## Le tableau ci-dessus est un prisme rectangulaire 3 unités
+## de haut, 4 de large et 5 de profond. Indicer ce prisme avec
+## un seul indice équivaut à en extraire des «tranches», alors
+## qu'utiliser deux indices équivaut à en tirer des «carottes»
+## (au sens géologique du terme). Il est laissé en exercice de
+## généraliser à plus de dimensions...
+x                          # les cinq matrices
+x[, , 1]                   # tranche transversale
+x[, 1, ]                   # tranche verticale
+x[1, , ]                   # tranche horizontale
+x[, 1, 1]                  # carotte de haut en bas
+x[1, 1, ]                  # carotte de devant à derrière
+x[1, , 1]                  # carotte de gauche à droite
+x[1, 1, 1]                 # donnée unique
+
+###
+### LISTE
+###
+
+## La liste est l'objet le plus général en R. C'est un objet
+## récursif qui peut contenir des objets de n'importe quel
+## mode (y compris la liste) et de n'importe quelle longueur.
+(x <- list(joueur = c("V", "C", "C", "M", "A"),
+           score = c(10, 12, 11, 8, 15),
+           expert = c(FALSE, TRUE, FALSE, TRUE, TRUE),
+           niveau = 2))
+is.vector(x)               # liste est un vecteur...
+is.recursive(x)            # ... récursif...
+length(x)                  # ... de quatre éléments...
+mode(x)                    # ... de mode "list"
+
+## Comme tout autre vecteur, une liste peut être concaténée
+## avec un autre vecteur avec la fonction 'c'.
+y <- list(TRUE, 1:5)       # liste de deux éléments
+c(x, y)                    # liste de six éléments
+
+## Les crochets simples [ ] permettent d'extraire un ou
+## plusieurs éléments d'une liste. Le résultat est toujours
+## une liste, même si l'on extraie un seul élément.
+x[c(1, 2)]                 # deux premiers éléments
+x[1]                       # premier élément: une liste
+
+## Lorsque l'on veut extraire un, et un seul, élément d'une
+## liste et obtenir l'objet lui-même (et non une liste
+## contenant l'objet), il faut utiliser les crochets doubles
+## [[ ]].
+x[[1]]                     # comparer avec ci-dessus
+
+## Jolie fonctionnalité: les crochets doubles permettent
+## d'indicer récursivement la liste, c'est-à-dire d'extraire
+## un objet de la liste, puis un élément de l'objet, et ainsi
+## de suite.
+x[[1]][2]                  # 2e élément du 1er élément
+x[[c(1, 2)]]               # idem, par indiçage récursif
+
+## Les éléments d'une liste étant généralement nommés (c'est
+## une bonne habitude à prendre!), il est souvent plus simple
+## et, surtout, plus sûr d'extraire les éléments d'une liste
+## par leur étiquette avec l'opérateur $.
+x$joueur                   # équivalent à x[[1]]
+x$joueur[2]                # équivalent à x[[c(1, 2)]]
+x[["expert"]]              # aussi valide, mais peu usité
+x$level <- 1               # aussi pour l'affectation
+
+## Une liste peut contenir n'importe quoi...
+x[[5]] <- matrix(1, 2, 2)  # ... une matrice...
+x[[6]] <- list(0:5, TRUE)  # ... une autre liste...
+x[[7]] <- seq              # ... même le code d'une fonction!
+x                          # eh ben!
+x[[c(6, 1, 3)]]            # de quel élément s'agit-il?
+
+## Il est possible de supprimer un élément d'une liste en lui
+## affectant la valeur 'NULL'.
+x[[7]] <- NULL; length(x)  # suppression du 7e élément
+
+## Il est parfois utile de convertir une liste en un simple
+## vecteur. Les éléments de la liste sont alors «déroulés», y
+## compris la matrice en position 5 dans notre exemple (qui
+## n'est rien d'autre qu'un vecteur, on s'en souviendra).
+unlist(x)                    # remarquer la conversion
+unlist(x, recursive = FALSE) # ne pas appliquer aux sous-listes
+unlist(x, use.names = FALSE) # éliminer les étiquettes
+
+###
+### FACTEUR
+###
+
+## Les facteurs jouent un rôle important en analyse de
+## données. surtout pour classer des données en diverses
+## catégories. Les données d'un facteur devraient normalement
+## afficher un fort taux de redondance.
+##
+## Reprenons l'exemple du chapitre.
+(grandeurs <-
+     factor(c("S", "S", "L", "XL", "M", "M", "L", "L")))
+levels(grandeurs)          # catégories
+as.integer(grandeurs)      # représentation interne
+
+## Dans le présent exemple, nous pourrions souhaiter que R
+## reconnaisse le fait que S < M < L < XL. C'est possible avec
+## les facteurs *ordonnés*.
+factor(c("S", "S", "L", "XL", "M", "M", "L", "L"),
+       levels = c("S", "M", "L", "XL"),
+       ordered = TRUE)
+
+###
+### DATA FRAME
+###
+
+## Un data frame est une liste dont les éléments sont tous de
+## la même longueur. Il comporte un attribut 'dim', ce qui fait
+## qu'il est représenté comme une matrice. Cependant, les
+## colonnes peuvent être de modes différents.
+data.frame(Nom = c("Pierre", "Jean", "Jacques"),
+           Age = c(42, 34, 19),
+           Fumeur = c(TRUE, TRUE, FALSE),
+           stringsAsFactors = FALSE)
+
+## R est livré avec plusieurs jeux de données, la plupart sous
+## forme de data frames.
+data()                     # liste complète
+
+## Création d'une copie du jeu de données 'USArrests' dans
+## l'espace de travail.
+data(USArrests)
+
+## Analyse succincte de l'objet.
+mode(USArrests)            # un data frame est une liste...
+length(USArrests)          # ... de quatre éléments...
+class(USArrests)           # ... de classe 'data.frame'
+dim(USArrests)             # dimensions implicites
+names(USArrests)           # titres des colonnes
+row.names(USArrests)       # titres des lignes
+USArrests[, 1]             # première colonne
+USArrests$Murder           # idem, plus simple
+USArrests[1, ]             # première ligne
+
+## La fonction 'subset' permet d'extraire des lignes et des
+## colonnes d'un data frame de manière très intuitive.
+##
+## Par exemple, nous pouvons extraire ainsi le nombre
+## d'assauts dans les états comptant un taux de meurtre
+## supérieur à 10.
+subset(USArrests, Murder > 10, select = Assault)
