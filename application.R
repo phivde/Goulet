70260c13db104e7c12b343bd04769bf773b5b801
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2017-11-06 14:58:44 -0500

    correction des entrées d'index de .GlobalEnv et .libPaths ->
    tri sans le . initial

---
+++ application.R
@@ -0,0 +1,15 @@
+### Emacs: -*- coding: utf-8; fill-column: 62; comment-column: 27; -*-
+##
+## Copyright (C) 2017 Vincent Goulet
+##
+## Ce fichier fait partie du projet «Programmer avec R»
+## http://github.com/vigou3/programmer-avec-r
+##
+## Cette création est mise à disposition selon le contrat
+## Attribution-Partage dans les mêmes conditions 4.0
+## International de Creative Commons.
+## http://creativecommons.org/licenses/by-sa/4.0/
+
+
+## bouts de operateurs.R sur l'utilisation de %% dans boucles et de nombres dans if()
+
