7ac16f22711b5174bd231a5e3e072d5eb4a78bbe
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2018-06-27 17:58:44 -0400

    notices de copyright en entête

--- collaboration.R
+++ collaboration.R
@@ -1,14 +1,15 @@
 ### Emacs: -*- coding: utf-8; fill-column: 62; comment-column: 27; -*-
 ##
-## Copyright (C) 2017 Vincent Goulet
+## Copyright (C) 2018 Vincent Goulet
 ##
-## Ce fichier fait partie du projet «Programmer avec R»
-## http://github.com/vigou3/programmer-avec-r
+## Ce fichier fait partie du projet
+## «Programmer avec R»
+## https://gitlab.com/vigou3/programmer-avec-r
 ##
 ## Cette création est mise à disposition selon le contrat
 ## Attribution-Partage dans les mêmes conditions 4.0
 ## International de Creative Commons.
-## http://creativecommons.org/licenses/by-sa/4.0/
+## https://creativecommons.org/licenses/by-sa/4.0/
 
 ###
 ### INTRODUCTION
