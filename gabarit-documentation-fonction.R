b0eda77318e26684ef7349a6f2f6b86791caa88a
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2018-11-23 16:25:38 -0500

    ajout d'un gabarit pour la documentation des fonctions

---
+++ gabarit-documentation-fonction.R
@@ -0,0 +1,15 @@
+###
+### <signature de la fonction>
+###
+##  <description de ce que fait la fonction en une phrase>
+##
+##  Arguments
+##
+##  <argument>: <type (vecteur, liste, etc.), signification
+##               et valeurs admissibles, le cas échéant>
+##
+##  Valeur
+##
+##  <type (vecteur, liste, etc.) et valeur retournée par la
+##   fonction>
+##
